cc83d29049ee0a92a6af637bca4399ec52358017
Juuso Murto <juusomurto@kamk.fi>
2020-09-14 23:22:10 +0300

    base ready for virtual workspace, movement, players connecting works

---
+++ VirtualWorkspace/Assets/Resources/Scripts/PlayerControl.cs
@@ -0,0 +1,17 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : NetworkBehaviour
+{
+    public float speed = 30;
+    public Rigidbody2D rigidbody2d;
+
+
+    void FixedUpdate()
+    {
+        if (isLocalPlayer)
+            rigidbody2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
+    }
+}
